c04af148839868e05b83b0eb8a750473f3837462
MaximilianoT13 <gmtarcaya@gmail.com>
2025-05-21 22:17:50 -0300

    avances en funciones para guardar y editar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useCallback,useEffect } from 'react'
 import './App.css'
 import FormularioProducto from './components/ProductForm'
+import ListaProductos from './components/ProductList'
+import ProductosItems from './components/ProductItem'
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
+const [productos,setProductos]=useState([])
+const [modEdicion,setEdicion]=useState(null)
+
+useEffect(()=>{
+  console.log(productos);
+},[productos])
+
+
+const guardar= useCallback ((producto)=>{
+    if(modEdicion){
+       setProductos((prev)=>
+        prev.map((prod)=> (modEdicion.id === prod.id ? producto : prod)))
+       setEdicion(null);
+    }
+    else 
+    {
+        setProductos((prev) => [...prev, producto]);
+    }
+},[modEdicion])
+
+const editar= useCallback ((producto) =>{
+    console.log(`editando producto(ID): ${producto.id}`)
+    setEdicion(producto);
+},[])
+
+const eliminar= useCallback ((id) =>{
+  
+   setProductos(prev=>prev.filter(prod => prod.id !==id));
+
+},[]) 
+
+
 
   return (
     <>
-      <div>
-      <FormularioProducto/>
-      </div>
+
+    <div>
+        <FormularioProducto guardar={guardar} prodEdit={modEdicion}/>
+        <ListaProductos productos={productos} eliminar={eliminar} editar={editar}/>
+    </div>
+
     
     </>
   )
